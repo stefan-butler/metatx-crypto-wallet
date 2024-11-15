8b96f4fb34c545530b0b5df1fe934fc60d45ea35
imakoug <pats.s1889@gmail.com>
2024-11-15 10:56:47 +0100

    index popup vite refactor

---
+++ client/src/popup.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root')!);
+root.render(<App />);
+
+document.addEventListener('DOMContentLoaded', () => {
+    chrome.runtime.sendMessage({ type: "CHECK_BALANCE", address: "your_test_address" }, (response) => {
+      console.log("Response from background:", response);
+    });
+  });
