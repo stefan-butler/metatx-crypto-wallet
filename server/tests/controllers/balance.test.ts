b9125fa6e5d617a118cbfc319e1e79f5bf32c869
Stefan Butler <stefanbutler@icloud.com>
2024-11-19 10:51:16 +0000

    test: transfer balance

--- server/tests/controllers/balance.test.ts
+++ server/tests/controllers/balance.test.ts
@@ -1,3 +1,51 @@
+import request from 'supertest';
+import app from '../../src/app';
+import { describe, beforeAll, it, afterAll, expect, jest } from '@jest/globals';
+import { InfuraProvider, ethers, TransactionResponse } from 'ethers';
+
 // fetchBalance
 
 // transferBalance
+jest.mock('ethers', () => {
+  //jest.requireActual imports the real ethers module
+  const originalModule = jest.requireActual<typeof ethers>('ethers');
+
+  // in return replace InfuraProvider with a mocked version. 
+  return {
+    ...originalModule,
+    InfuraProvider: jest.fn().mockImplementation(() => ({
+      getBalance: jest.fn(),
+      sendTransaction: jest.fn<() => Promise<any>>().mockResolvedValue({
+        hash: '0x123',
+        wait: jest.fn(),
+      }), 
+    })),
+  };
+});
+
+describe('Transfer Balance Controller', ()=> {
+  it('should successfully transfer balance', async() => {
+    const response = await request(app)
+      .post('/transfer')
+      .send({
+        recipient: '0x742d35Cc6634C0532925a3b844Bc454e4438f44e',
+        amount: '0.01'
+      });
+      console.log(response.body);
+      expect(response.status).toBe(200);
+      expect(response.body.message).toBe('Transfer successful!');
+      expect(response.body.txHash).toBe('0x123');
+  }, 30000);
+
+  it('should handle transfer failure', async() => {
+    const response = await request(app)
+      .post('/transfer')
+      .send({
+        recipient: '0xInvalidAddress',
+        amount: '0.01'
+      })
+
+      expect(response.status).toBe(500);
+      expect(response.body.message).toBe('Transfer failed');
+  })
+});
--- server/tests/index.test.ts
+++ server/tests/index.test.ts
@@ -2,7 +2,8 @@ import request from 'supertest';
 import mongoose from 'mongoose';
 import { MongoMemoryServer } from 'mongodb-memory-server';
 import app from '../src/app';
-import { describe, beforeAll, it, afterAll, expect } from '@jest/globals';
+import { describe, beforeAll, it, afterAll, expect, jest } from '@jest/globals';
+import { InfuraProvider, ethers, TransactionResponse } from 'ethers';
 
 describe('App', () => {
   let mongoServer: MongoMemoryServer;
@@ -27,4 +28,22 @@ describe('App', () => {
       'http://localhost:3000'
     );
   });
+});
+
+// transferBalance
+jest.mock('ethers', () => {
+  //jest.requireActual imports the real ethers module
+  const originalModule = jest.requireActual<typeof ethers>('ethers');
+
+  // in return replace InfuraProvider with a mocked version. 
+  return {
+    ...originalModule,
+    InfuraProvider: jest.fn().mockImplementation(() => ({
+      getBalance: jest.fn(),
+      sendTransaction: jest.fn().mockResolvedValue({
+        hash: '0x123',
+        wait: jest.fn(),
+      } as never), 
+    })),
+  };
 });
